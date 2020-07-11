14ec8a3475309bd9a5ca43a7dc23b4e230037107
Brian on VM <bvalette@student.42.fr>
2020-07-11 10:31:32 +0200

    supervisor on ftps container, with kill.sh to kill supervisor if any of the subprocess dies

--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 echo -e "$USER_PASS_VAL\n$USER_PASS_VAL" | adduser -h /tmp ftp42 && export USER_PASS_VAL=0
-exec /usr/sbin/pure-ftpd
+#exec /usr/sbin/pure-ftpd
+
+supervisord -c /etc/supervisor.conf
+
+
