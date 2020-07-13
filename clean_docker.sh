4ac24979118af482f97539a166fa9ce27c699f2b
Brian on VM <bvalette@student.42.fr>
2020-07-13 15:08:27 +0200

    ftps ok with tls/ssl

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,4 +1,8 @@
 # !/bin/sh
 
+docker kill $(docker ps -aq)
 docker rmi --force $(docker images --quiet --filter "label=project=ft_service")
 echo -e Y | docker system prune
+echo -e Y | docker volume prune
+docker system df
+
--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -3,6 +3,5 @@
 echo -e "$USER_PASS_VAL\n$USER_PASS_VAL" | adduser -h /tmp ftp42 && export USER_PASS_VAL=0
 #exec /usr/sbin/pure-ftpd
 
-supervisord -c /etc/supervisor.conf
-
+supervisord
 
