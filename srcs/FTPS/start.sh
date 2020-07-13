867f10aa045479d78d0f9ec080235ab4fbefe280
Brian on VM <bvalette@student.42.fr>
2020-07-13 15:22:56 +0200

    cleaned ftp conf file and create home folder for userW

--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo -e "$USER_PASS_VAL\n$USER_PASS_VAL" | adduser -h /tmp ftp42 && export USER_PASS_VAL=0
+echo -e "$USER_PASS_VAL\n$USER_PASS_VAL" | adduser -h /home/ftp ftp42 && export USER_PASS_VAL=0
 #exec /usr/sbin/pure-ftpd
 
 supervisord
