c481ee944613909a2bba6ddca3bd9ccdd96c2f29
Brian on VM <bvalette@student.42.fr>
2020-07-14 17:53:25 +0200

    telegraf ok (outside k8s, see if it works inside)

--- build_images.sh
+++ build_images.sh
@@ -17,18 +17,16 @@ docker volume create --name ftps_data --label project=ft_service --label service
 docker build -t ft_service_alpine srcs/base_image
 
 
-docker build -t ft_service/ftps srcs/FTPS/
-docker run -d --rm -it -v ftps_data:/mnt/ftps_data -e USER_PASS_VAL=$USER_PASS ft_service/ftps
-
 #docker build -t ft_service/nginx srcs/Nginx
-#docker run -it ft_service/nginx
-
+docker build -t ft_service/ftps srcs/FTPS/
 docker build -t ft_service/influxdb srcs/InfluxDB
-docker run -d --rm -it -v influxdb_db:/mnt/db -v influxdb_influx:/mnt/influx ft_service/influxdb
-
 #docker build -t ft_service/Grafana srcs/Grafana
 #docker build -t ft_service/LoadBalancer srcs/LoadBalancer
 #docker build -t ft_service/MySQL srcs/MySQL
 #docker build -t ft_service/PhpMyAdmin srcs/PhpMyAdmin
 #docker build -t ft_service/WordPress srcs/WordPress
+
+#docker run -it ft_service/nginx
+docker run -d --rm -it --name ftps -v ftps_data:/mnt/ftps_data -e USER_PASS_VAL=$USER_PASS ft_service/ftps
+docker run -d --rm -it --name influxdb -v influxdb_db:/mnt/db -v influxdb_influx:/mnt/influx ft_service/influxdb
 
