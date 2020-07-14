a0ffd3e2001c03dc8c236d6b512757092532aeba
Brian on VM <bvalette@student.42.fr>
2020-07-14 14:19:10 +0200

    added volumes for ftps and influxdb containers, improved script for building images

--- build_images.sh
+++ build_images.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 #eval $(minikube -p minikube docker-env)
-#echo "Hello ! Let's set a few infos first :"
-#stty -echo
-#printf "Password: "
-#read USER_PASS
-#stty echo
-#printf "\n"
-#echo "Thanks !"
-#export USER_PASS=$USER_PASS
+echo "Hello ! Let's set a few infos first :"
+stty -echo
+printf "Password: "
+read USER_PASS
+stty echo
+printf "\n"
+echo "Thanks !"
+export USER_PASS=$USER_PASS
 
+docker volume create --name influxdb_influx --label project=ft_service --label service=influxdb
+docker volume create --name influxdb_db --label project=ft_service --label service=influxdb
+docker volume create --name ftps_data --label project=ft_service --label service=ftps
 
 docker build -t ft_service_alpine srcs/base_image
 
 
-#docker build -t ft_service/ftps srcs/FTPS/
+docker build -t ft_service/ftps srcs/FTPS/
+docker run -d --rm -it -v ftps_data:/mnt/ftps_data -e USER_PASS_VAL=$USER_PASS ft_service/ftps
 
-#docker run -e USER_PASS_VAL=$USER_PASS -it ft_service/ftps
+#docker build -t ft_service/nginx srcs/Nginx
+#docker run -it ft_service/nginx
 
+docker build -t ft_service/influxdb srcs/InfluxDB
+docker run -d --rm -it -v influxdb_db:/mnt/db -v influxdb_influx:/mnt/influx ft_service/influxdb
 
-
-docker build -t ft_service/nginx srcs/Nginx
-docker run -it ft_service/nginx
 #docker build -t ft_service/Grafana srcs/Grafana
-#docker build -t ft_service/InfluxDB srcs/InfluxDB
 #docker build -t ft_service/LoadBalancer srcs/LoadBalancer
 #docker build -t ft_service/MySQL srcs/MySQL
 #docker build -t ft_service/PhpMyAdmin srcs/PhpMyAdmin
