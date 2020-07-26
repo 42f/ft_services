7349de11dbbc96882beb063e263ff4cdb09941f3
Brian on VM <bvalette@student.42.fr>
2020-07-26 14:26:35 +0200

    set secrets to carry password to the container

--- build_images.sh
+++ build_images.sh
@@ -4,33 +4,17 @@ GREEN='\033[1;32m'
 DARK='\033[1;93m'
 RESET='\033[0m'
 
-#eval $(minikube -p minikube docker-env)
-#echo -e "${GREEN}Please set a password for services users:${RESET}"
-#stty -echo
-#printf "Password: "
-#read USER_PASS
-##stty echo
-#printf "\n"
-#echo "Thanks !"
-#export USER_PASS=$USER_PASS
+eval $(minikube -p minikube docker-env)
+echo -e "${GREEN}Please set a password for services users:${RESET}"
+stty -echo
+printf "Password: "
+read USER_PASS
+#stty echo
+printf "\n"
+echo "Thanks !"
 
-
-
-create_volume () {
-	docker volume ls --quiet --filter name=$1 | grep $1
-	if [ $? -ne 0 ]
-		then
-			printf "‼️⚡️ ${RED} Volume $1 missing, creating it...\n${RESET}"
-			docker volume create --name $1 --label project=ft_service --label service=$2
-		else
-			printf "👌 ${GREEN} Volume $1 fund, skiping creation...\n${RESET}"
-	fi
-}
-
-create_volume mysql-db mysql
-create_volume influxdb-influx influxdb
-create_volume influxdb-db influxdb
-create_volume ftps-data ftps
+kubectl delete secret mdpftp
+kubectl create secret generic mdpftp --from-literal=pass=$USER_PASS
 
 docker build -t ft_service_alpine srcs/base_image
 
--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-export USER=ftp42
-echo -e "42\n42" | adduser -h /mnt/ftp ftp42
-#echo -e "$USER_PASS_VAL\n$USER_PASS_VAL" | adduser -h /mnt/ftps_data ftp42
+#export USER=ftp42
+#echo -e "42\n42" | adduser -h /mnt/ftp ftp42
+echo -e "$SECRET_FTP\n$SECRET_FTP" | adduser -h /mnt/ftp ftp42
+unset SECRET_FTP
 
 touch /mnt/ftp/Hellow_from_ftp_volume
 mkdir -p /mnt/ftp/ftp_boring_dir
